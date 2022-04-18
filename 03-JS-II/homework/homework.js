3aa87cf80e25ea78c771abe631e015eff1851ea1
PATOBZ <102373806+PATOBZ@users.noreply.github.com>
2022-04-18 10:56:01 -0300

    homeworks02

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -151,7 +151,6 @@ function esPrimo(numero) {
   }
   return true;
 }
-}
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
@@ -167,19 +166,31 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  let array = []
+  for (i=0; i<=10; i++){
+  array.push (6*i);
+  }
+return array 
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero < 1000 && numero > 99) {
+    return true
+  }
+  return false
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  let funcion = 0
+  let resultado = numero
+  do {resultado += 5; funcion++}
+     while (funcion < 8);
+     return resultado
 }
 
 
