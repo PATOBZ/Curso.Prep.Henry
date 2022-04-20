79dd2ccabad612475c50c7164914dc659f9459a0
PATOBZ <102373806+PATOBZ@users.noreply.github.com>
2022-04-20 17:51:58 -0300

    homeworks03

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -216,15 +216,19 @@ function breakStatement(numero) {
   //Pista: usá el statement 'break'
   // Tu código:
   var nuevoValor = []
-  for (var=0; i < 10; i++){
+  for (var i=0; i < 10; i++){
     numero = numero +2
     if (numero === i){
       break
     }
     else (nuevoValor.push(numero))
   }
-  if ()
+  if (i<10){
   return "Se interrumpió la ejecución"
+}
+  else {
+    return nuevoValor
+  }
 }
 
 
@@ -235,7 +239,18 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for(var i= 0; i<10; i++) {
+    if(i === 5) continue;
+    else {
+      suma = suma + 2;
+      array.push(suma);
+    }
+  }
+  return array;
 }
+
 
 
 // No modificar nada debajo de esta línea
